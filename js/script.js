72c48cb9b24035e66c4feb5ea442c193579eb8a8
Alex Borisevych <santos2003310@gmail.com>
2022-11-07 19:08:48 +0200

    222222222222

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
-const swiper1= new Swiper('.swiper2',{
+const swiper1= new Swiper('.swiper1',{
     slidesPerView:4,
     slidesPerGroup:1,
     centeredSlides:true,
    
   
+});
+const swiper2 = new Swiper('.swiper2',{
+    slidesPerView:4,
+    
+
+});
+const swiper3 = new Swiper('.swiper3',{
+    slidesPerView:4,
+    
+
 })
