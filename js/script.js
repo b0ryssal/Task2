f3d7589fe4866807966fcd2d7762a105193ab4dc
Alex Borisevych <santos2003310@gmail.com>
2022-11-17 15:18:02 +0200

    adaptive commit

--- js/script.js
+++ js/script.js
@@ -6,12 +6,36 @@ const swiper1= new Swiper('.swiper1',{
   
 });
 const swiper2 = new Swiper('.swiper2',{
-    slidesPerView:4,
+    
+    breakpoints:{
+        1452:{
+            slidesPerView:4,
+
+
+        },
+        768:{
+            slidesPerView:2.5,
+
+
+        },
+    }
     
 
 });
 const swiper3 = new Swiper('.swiper3',{
     slidesPerView:4,
+    breakpoints:{
+        1452:{
+            slidesPerView:4,
+
+
+        },
+        768:{
+            slidesPerView:2.5,
+
+
+        },
+    }
     
 
 })
@@ -27,3 +51,12 @@ const swiper4 = new Swiper('.hero__swiper',{
     
 
 })
+const burger = document.querySelector('.header__burger');
+if(burger){
+    const menuBody=document.querySelector('.header__bar');
+    burger.addEventListener("click", function(e){
+        document.body.classList.toggle('_lock');
+        burger.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    });
+}
